66075208052235d52a586b6ed8609220b9f3426d
Zodiaco1999 <brayitanbaez@gmail.com>
2026-03-10 17:41:51 -0500

    Se ajusto la logica del movimiente del rey

--- ajedrez.py
+++ ajedrez.py
@@ -4,6 +4,41 @@ fila_p = []
 fila_i = []
 tablero = []
 tablero_vacio = []
+columnas_a_indices = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7
+}
+filas_a_indices = {
+    '1': 7,
+    '2': 6,
+    '3': 5,
+    '4': 4,
+    '5': 3,
+    '6': 2,
+    '7': 1,
+    '8': 0
+} 
+x = 0
+y = 0
+turno = 1
+turno_inv = 0
+pieza_selec = ""
+primer_mov_bp = False
+primer_mov_np = False
+enroque_br = False
+enroque_nr = False
+primer_mov_nr = False
+primer_mov_ntd = False
+primer_mov_nti = False
+primer_mov_br = False
+primer_mov_btd = False
+primer_mov_bti = False
 
 # tablero = [[N for i in range(8) if i % 2 == 0] for j in range(8)]
 
@@ -33,95 +68,49 @@ def imprimir_tablero():
         print(8 - i,tablero[i][:],8 - i)
     nomenclatura_columnas()
 
-# Asignando las piezas a las posciciones de la matris bidimensional 
-tablero[0][0] = tablero[0][7] = TN
-tablero[0][1] = tablero[0][6] = CN
-tablero[0][2] = tablero[0][5] = AN
-tablero[0][3] = DN
-tablero[0][4] = RN
-tablero[7][0] = tablero[7][7] = TB
-tablero[7][1] = tablero[7][6] = CB
-tablero[7][2] = tablero[7][5] = AB
-tablero[7][3] = DB
-tablero[4][3] = RB
-
-for i in range(8):
-    tablero[6][i] = PB
-    tablero[1][i] = PN
-
-filas_t = "12345678"
-columnas_t = "abcdefgh"
-x = 0
-y = 0
-turno = 1
-turno_inv = 0
-pieza_selec = ""
+# Asignando las piezas a las posciciones de la matriz bidimensional 
+tablero[0][0] = tablero[0][7] = NT
+tablero[0][1] = tablero[0][6] = NC
+tablero[0][2] = tablero[0][5] = NA
+tablero[0][3] = ND
+tablero[0][4] = NR
+tablero[7][0] = tablero[7][7] = BT
+# tablero[7][1] = tablero[7][6] = BC
+# tablero[7][2] = tablero[7][5] = BA
+# tablero[7][3] = BD
+tablero[7][4] = BR
+tablero[6] = [BP] * 8
+tablero[1] = [NP] * 8
 
 def msj_posicion_inv():
     print("\n¡Posición invalida!\n")
     input("Presione enter para continuar")
     
-def validar_posicion(p):
-    global x
-    global y 
-    contador = 0
-    for i in p:
-        if contador == 0:
-            if i in columnas_t:
-                if i == "a":
-                    x = 0
-                elif i == "b":
-                    x = 1
-                elif i == "c":
-                    x = 2
-                elif i == "d":
-                    x = 3
-                elif i == "e":
-                    x = 4
-                elif i == "f":
-                    x = 5
-                elif i == "g":
-                    x = 6
-                elif i == "h":
-                    x = 7
-            else:
-                return False
-        else:
-            if i in filas_t:
-                if i == "1":
-                    y = 7
-                elif i == "2":
-                    y = 6
-                elif i == "3":
-                    y = 5
-                elif i == "4":
-                    y = 4
-                elif i == "5":
-                    y = 3
-                elif i == "6":
-                    y = 2
-                elif i == "7":
-                    y = 1
-                elif i == "8":
-                    y = 0
-            else:
-                return False       
-        contador += 1
+def es_posicion_valida(p):
+    if len(p) != 2:
+        return False
+    
+    global x, y
+    p = p.lower()
+    
+    x = columnas_a_indices.get(p[0])
+    y = filas_a_indices.get(p[1])
+    
+    if x is None or y is None:
+        return False
+    
     return True
 
-primer_mov_pb = 0
-primer_mov_pn = 0
-
 def turnos(t, ti=1):
-    global primer_mov_pb
-    global primer_mov_pn 
+    global primer_mov_bp
+    global primer_mov_np 
     if ti:
         if t % 2 != 0:
             print("\n=============Turno de las blancas===============")
-            primer_mov_pb = 0
+            primer_mov_bp = False
         else:
             print("\n=============Turno de las negras================")
-            primer_mov_pn = 0
+            primer_mov_np = False
     else:
         if t % 2 != 0:
             print("\n¡Por favor, seleccione una pieza blanca!\n")
@@ -130,6 +119,79 @@ def turnos(t, ti=1):
             print("\n¡Por favor, seleccione una pieza negra!\n")
             input("Presione enter para continuar\n")
             
+def es_pieza_actual(pieza):
+    return pieza_selec == pieza
+            
+def mov_peon_blanco():
+    global primer_mov_bp, primer_mov_np 
+    incr_xv = xv + 1
+    decr_xv = xv - 1
+
+    if xv == 7:
+        incr_xv = 7
+    if xv == 0:
+        decr_xv = 0
+    
+    # Validación captura el paso peón blanco
+    if yv == 3 and tablero[yv][incr_xv] == NP and primer_mov_np:
+        if y == 2 and incr_xv == x:
+            tablero[yv][incr_xv] = tablero_vacio[yv][incr_xv]
+            return True
+    if yv == 3 and tablero[yv][decr_xv] == NP and primer_mov_np:
+        if y == 2 and decr_xv == x:
+            tablero[yv][decr_xv] = tablero_vacio[yv][decr_xv]
+            return True
+    # --------------------------------------
+    if yv in range(6):
+        if yv - 1 == y and xv == x and tablero[y][x] not in all_piezas:  
+            return True
+        elif yv - 1 == y and xv + 1 == x or xv - 1 == x:
+            if tablero[y][x] in piezas_negras_sr:
+                return True
+    else:
+        if ((yv - 2 == y or yv - 1 == y) and xv == x) and tablero[y][x] not in all_piezas:
+            if y == 4:
+                primer_mov_bp = True     
+            return True    
+        if yv - 1 == y and (incr_xv == x or xv - 1 == x):
+            if tablero[y][x] in piezas_negras_sr:
+                return True
+        
+def mov_peon_negro():
+    global primer_mov_bp, primer_mov_np 
+    incr_xv = xv + 1
+    decr_xv = xv - 1
+
+    if xv == 7:
+        incr_xv = 7
+    if xv == 0:
+        decr_xv = 0
+    
+    # Validación captura el paso peón negro 
+    if yv == 4 and tablero[yv][incr_xv] == BP and primer_mov_bp:
+        if y == 5 and incr_xv == x:
+            tablero[yv][incr_xv] = tablero_vacio[yv][incr_xv]
+            return True
+    if yv == 4 and tablero[yv][decr_xv] == BP and primer_mov_bp:
+        if y == 5 and decr_xv == x:
+            tablero[yv][decr_xv] = tablero_vacio[yv][decr_xv]
+            return True
+    # -------------------------------------    
+    if yv in range(2, 8):
+        if yv + 1 == y and xv == x and tablero[y][x] not in all_piezas:  
+           return True
+        elif yv + 1 == y and xv + 1 == x or xv - 1 == x:
+           if tablero[y][x] in piezas_blancas_sr:
+               return True    
+    else:
+        if ((yv + 2 == y or yv + 1 == y) and xv == x) and tablero[y][x] not in all_piezas: 
+            if y == 3:
+                primer_mov_np = True 
+            return True    
+        if yv + 1 == y and (xv + 1 == x or xv - 1 == x):
+            if tablero[y][x] in piezas_blancas_sr:
+                return True   
+         
 def mov_caballo():
     if ((xv + 2 == x or xv - 2 == x) and (yv - 1 == y or yv + 1 == y)) or ((xv + 1 == x or xv - 1 == x) and (yv - 2 == y or yv + 2 == y)):
         return True
@@ -173,8 +235,6 @@ def mov_alfil():
             ciy += 1 
             cdx -= 1
 
-# def recorrido_torre(list_cy, list_cx):
-    
 def mov_torre():
     list_cy, list_cx  = [], []
     cy, cx =  yv, xv 
@@ -214,104 +274,75 @@ def mov_torre():
                         return False 
                 return True 
             cx -= 1  
-    
-def identificacion_pieza_selec(p, xv, yv, x, y):
-    global primer_mov_pb
-    global primer_mov_pn 
-    
-    incr_xv = xv + 1
-    decr_xv = xv - 1
+ 
+def mov_reina():
+    return mov_alfil() or mov_torre()
 
-    if xv == 7:
-        incr_xv = 7
-    if xv == 0:
-        decr_xv = 0
-        
-    if p in piezas_blancas and tablero[y][x] not in piezas_blancas + RN:
-        # Validación movimiento de peón blanco           
-        if p == PB: 
-            # Validación captura el paso peón blanco
-            if yv == 3 and tablero[yv][incr_xv] == PN and primer_mov_pn == 1:
-                if y == 2 and incr_xv == x:
-                    tablero[yv][incr_xv] = tablero_vacio[yv][incr_xv]
-                    return True
-            if yv == 3 and tablero[yv][decr_xv] == PN and primer_mov_pn == 1:
-                if y == 2 and decr_xv == x:
-                    tablero[yv][decr_xv] = tablero_vacio[yv][decr_xv]
-                    return True
-            # --------------------------------------
-            if yv in range(6):
-                if yv - 1 == y and xv == x and tablero[y][x] not in all_piezas:  
-                    return True
-                elif yv - 1 == y and xv + 1 == x or xv - 1 == x:
-                    if tablero[y][x] in piezas_negras_sr:
-                        return True
-            else:
-                if ((yv - 2 == y or yv - 1 == y) and xv == x) and tablero[y][x] not in all_piezas:
-                    if y == 4:
-                        primer_mov_pb = 1     
-                    return True    
-                if yv - 1 == y and (incr_xv == x or xv - 1 == x):
-                    if tablero[y][x] in piezas_negras_sr:
-                        return True
-        # Validación movimiento de Caballo blanco
-        elif p == CB:
+def mov_rey_blanco():
+    global enroque_br, primer_mov_br
+    
+    if (xv + 1 == x or xv - 1 == x or xv == x) and (yv + 1 == y or yv - 1 == y or yv == y):
+        primer_mov_br = True
+        return True         
+    elif not enroque_br and not primer_mov_br and x == xv + 2 and tablero[y][xv + 3] == BT:
+        print("¡Enroque corto rey blanco!")
+        tablero[y][xv + 1] = BT
+        tablero[y][xv + 3] = tablero_vacio[y][xv + 3]
+        enroque_br = True
+        primer_mov_br = True
+        return True
+    elif not enroque_br and not primer_mov_br and x == xv - 2 and tablero[y][xv - 4] == BT:
+        print("¡Enroque largo rey blanco!")
+        tablero[y][xv - 1] = BT
+        tablero[y][xv - 4] = tablero_vacio[y][xv - 4]
+        enroque_br = True
+        primer_mov_br = True
+        return True
+    
+    return False
+
+def mov_rey_negro():
+    if (xv + 1 == x or xv - 1 == x or xv == x) and (yv + 1 == y or yv - 1 == y or yv == y):
+        return True         
+    
+    return False
+    
+def movimiento_pieza(p, x, y):
+    # movimientos de las piezas blancas
+    if p in piezas_blancas and tablero[y][x] not in piezas_blancas + NR:
+        if es_pieza_actual(BP): 
+            return mov_peon_blanco()
+        elif es_pieza_actual(BC):
             return mov_caballo()
-        # Validación movimiento de Alfil blanco  
-        elif p == AB:
+        elif es_pieza_actual(BA):
             return mov_alfil()
-        elif p == TB:
+        elif es_pieza_actual(BT):
             return mov_torre()    
-        elif p == DB:
-            return mov_alfil() or mov_torre()
-        elif p == RB:
-            if (xv + 1 == x or xv - 1 == x or xv == x) and (yv + 1 == y or yv - 1 == y or yv == y):
-                return True         
-    elif p in piezas_negras and tablero[y][x] not in piezas_negras + RB:
-        # Validación movimiento de Peón negro
-        if p == PN:
-            # Validación captura el paso peón negro 
-            if yv == 4 and tablero[yv][incr_xv] == PB and primer_mov_pb == 1:
-                if y == 5 and incr_xv == x:
-                    tablero[yv][incr_xv] = tablero_vacio[yv][incr_xv]
-                    return True
-            if yv == 4 and tablero[yv][decr_xv] == PB and primer_mov_pb == 1:
-                if y == 5 and decr_xv == x:
-                    tablero[yv][decr_xv] = tablero_vacio[yv][decr_xv]
-                    return True
-            # -------------------------------------    
-            if yv in range(2, 8):
-                if yv + 1 == y and xv == x and tablero[y][x] not in all_piezas:  
-                   return True
-                elif yv + 1 == y and xv + 1 == x or xv - 1 == x:
-                   if tablero[y][x] in piezas_blancas_sr:
-                       return True    
-            else:
-                if ((yv + 2 == y or yv + 1 == y) and xv == x) and tablero[y][x] not in all_piezas: 
-                    if y == 3:
-                        primer_mov_pn = 1 
-                    return True    
-                if yv + 1 == y and (xv + 1 == x or xv - 1 == x):
-                    if tablero[y][x] in piezas_blancas_sr:
-                        return True   
-        # Validación movimiento de Caballo negro    
-        elif p == CN:
+        elif es_pieza_actual(BD):
+            return mov_reina()
+        elif es_pieza_actual(BR):
+            return mov_rey_blanco()
+    # movimientos de las piezas negras        
+    elif p in piezas_negras and tablero[y][x] not in piezas_negras + BR:
+        if es_pieza_actual(NP):
+            return mov_peon_negro()
+        elif es_pieza_actual(NC):
             return mov_caballo()
-        elif p == AN:
+        elif es_pieza_actual(NA):
             return mov_alfil()
-        elif p == TN:
+        elif es_pieza_actual(NT):
             return mov_torre()   
-        elif p == DN:
-            return mov_alfil() or mov_torre()
-        elif p == RN:
-            if (xv + 1 == x or xv - 1 == x) or (yv + 1 == y or yv - 1 == y):
-                return True
+        elif es_pieza_actual(ND):
+            return mov_reina()
+        elif es_pieza_actual(NR):
+            return mov_rey_negro()
     else:
         return False
     
 def volver():
     imprimir_tablero()
     global p
+    print(f"\nPieza seleccionada: {pieza_selec}")
     p = input("\nIngrese la posición donde quiere mover la pieza o presione \"v\" para volver\n")
     if p == "v":
         global turno
@@ -330,21 +361,21 @@ while True:
 
     for y in range(8):
         for x in range(8):
-            if tablero[y][x] == RB:
+            if tablero[y][x] == BR:
                 ubic_rey = tablero[y][x]
                 break
         else:
             continue
         break
     
-    if tablero[y - 1][x + 1] == PN or tablero[y - 1][x - 1] == PN:
+    if tablero[y - 1][x + 1] == NP or tablero[y - 1][x - 1] == NP:
         print("¡Rey blanco en jaque!")
 
     turnos(turno)
-    p = input("\nIngrese la posición donde está actualmente la pieza que quiere seleccionar\n")
+    p = input("\nIngrese la posición de la pieza que quiere seleccionar\n")
     
-    if validar_posicion(p):
-        if tablero[y][x] == "◻ " or tablero[y][x] == "◼ ":
+    if es_posicion_valida(p):
+        if tablero[y][x] == B or tablero[y][x] == N:
             print("\n¡No ha seleccionado ninguna pieza!")
             input("\nPresione enter para continuar")
             continue
@@ -372,8 +403,7 @@ while True:
         continue
     
     while not volver():
-        if not validar_posicion(p) or not identificacion_pieza_selec(pieza_selec, xv, yv, x, y):
+        if not es_posicion_valida(p) or not movimiento_pieza(pieza_selec, x, y):
             msj_posicion_inv()
         else: break
     tablero[y][x] = pieza_selec
-    
